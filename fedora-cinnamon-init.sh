faf3021f27e96db5d0cb93a8ec58bc61cdb160b2
prodigylv <uldzerve@gmail.com>
2024-09-06 18:04:27 +0300

    Update fedora-cinnamon-init.sh
    fix bina bash

--- fedora-cinnamon-init.sh
+++ fedora-cinnamon-init.sh
@@ -1,4 +1,4 @@
-!#/bin/bash
+#!/bin/bash
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
