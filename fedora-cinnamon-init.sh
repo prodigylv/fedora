6e9ad92e783a7ae33db6d90843fc6555607e1dba
prodigylv <uldzerve@gmail.com>
2024-09-06 18:01:06 +0300

    Create fedora-cinnamon-init.sh
    test

---
+++ fedora-cinnamon-init.sh
@@ -0,0 +1,7 @@
+!#/bin/bash
+sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+sudo dnf install https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+
+sudo dnf install ntfs-3g
+sudo dnf install doublecmd-common doublecmd-gtk
